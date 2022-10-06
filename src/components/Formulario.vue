788ca7bb15ce8cd13c0ccd1bbd394659c495b306
VRCarlos21 <vazquezrosascarlosalexis@gmail.com>
2022-10-05 23:45:16 -0500

    Subiendo el 04/10/2022

---
+++ src/components/Formulario.vue
@@ -0,0 +1,41 @@
+<template>
+    <form @submit.prevent="registrarProyecto" >
+        <div class="mb-3">
+          <label class="form-label">Proyecto</label>
+          <input type="text" class="form-control" required />
+          
+        </div>
+
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Actividad</label>
+          <select class="form-select" required>
+            <option disabled value="">Seleccione una actividad</option>
+            <option disabled selected value="" >Aplicaciones Web con Vue.js</option>
+            <option>Backend Service con NOde.js</option>
+            <option>App Movil con React Native</option>
+          </select>
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-check-label">Urgente</label>
+            <input type="checkbox" class="form-check-input" >
+          </div>
+
+        
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
+
+</template>
+
+<script>
+    export default{
+        data:()=>({
+
+        }),
+        methods:{
+            registrarProyecto(){
+            console.log("Proyecto registrado...");
+            },
+        },
+    };
+</script>
